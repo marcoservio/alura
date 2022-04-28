0631fc620b94707a879fb13c5cc1a1cc3382df12
Marco Sérvio <marcoservio22@hotmail.com>
2022-04-28 17:46:57 -0300

    Lendo e Escrevendo arquivos csv e txt

---
+++ ByteBankImportacaoExportacao/ByteBankImportacaoExportacao/3_CriandoArquivos.cs
@@ -0,0 +1,38 @@
+﻿using ByteBankImportacaoExportacao.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO; // IO = Input e Output
+
+namespace ByteBankImportacaoExportacao
+{
+    partial class Program
+    {
+        static void CriarArquivo()
+        {
+            var caminhoNovoArquivo = "contasExportadas.csv";
+
+            using(var fluxoDeArquivo = new FileStream(caminhoNovoArquivo, FileMode.Create))
+            {
+                var contaComoString = "456,743633,4354.50,Gustavo,Santos";
+                var encoding = Encoding.UTF8;
+                var bytes = encoding.GetBytes(contaComoString);
+
+                fluxoDeArquivo.Write(bytes, 0, bytes.Length);
+            }
+        }
+
+        static void CriarArquivoComWrite()
+        {
+            var caminhoNovoArquivo = "contasExportadas.csv";
+
+            using(var fluxoDeArquivo = new FileStream(caminhoNovoArquivo, FileMode.Create))
+            using(var escritor = new StreamWriter(fluxoDeArquivo, Encoding.UTF8))
+            {
+                escritor.Write("312,312312,312.90,Pedro");
+            }
+        }
+    }
+}
---
+++ ByteBankImportacaoExportacao/ByteBankImportacaoExportacao/Program.cs
@@ -0,0 +1,21 @@
+﻿using ByteBankImportacaoExportacao.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO; // IO = Input e Output
+
+namespace ByteBankImportacaoExportacao
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            CriarArquivoComWrite();
+            Console.WriteLine("Aplicação finalizada...");
+
+            Console.ReadLine();
+        }
+    }
+}
