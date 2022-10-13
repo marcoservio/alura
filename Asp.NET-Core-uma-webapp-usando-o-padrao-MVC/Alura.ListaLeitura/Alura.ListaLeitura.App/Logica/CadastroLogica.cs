75cde2d1f38858efc35655d6fd892a372812aaa7
Marco Sérvio <marcoservio22@hotmail.com>
2022-10-13 17:27:42 -0300

    Initial commit

---
+++ Asp.NET-Core-uma-webapp-usando-o-padrao-MVC/Alura.ListaLeitura/Alura.ListaLeitura.App/Logica/CadastroLogica.cs
@@ -0,0 +1,26 @@
+﻿using Alura.ListaLeitura.App.HTML;
+using Alura.ListaLeitura.App.Negocio;
+using Alura.ListaLeitura.App.Repositorio;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Alura.ListaLeitura.App.Logica
+{
+    public class CadastroController
+    {
+        public string Incluir(Livro livro)
+        {
+            var repo = new LivroRepositorioCSV();
+            repo.Incluir(livro);
+            return "O livro foi adicionado com sucesso";
+        }
+
+        public IActionResult ExibeFormulario()
+        {
+            //var html = HtmlUtils.CarregaArquivoHTML("Formulario");
+            var html = new ViewResult { ViewName = "formulario" };
+            return html;
+        }
+    }
+}
---
+++ Asp.NET-Core-uma-webapp-usando-o-padrao-MVC/Alura.ListaLeitura/Alura.ListaLeitura.App/Logica/LivrosLogica.cs
@@ -0,0 +1,45 @@
+﻿using Alura.ListaLeitura.App.HTML;
+using Alura.ListaLeitura.App.Negocio;
+using Alura.ListaLeitura.App.Repositorio;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Alura.ListaLeitura.App.Logica
+{
+    public class LivrosController : Controller
+    {
+        private LivroRepositorioCSV _repo = new LivroRepositorioCSV();
+
+        public string Detalhes(int id)
+        {
+            var livro = _repo.Todos.First(l => l.Id == id);
+            return livro.Detalhes();
+        }
+
+        public IActionResult ParaLer()
+        {
+            ViewBag.Livros = _repo.ParaLer.Livros;
+            return View("lista");
+        }
+
+        public IActionResult Lendo()
+        {
+            ViewBag.Livros = _repo.Lendo.Livros;
+            return View("lista");
+        }
+
+        public IActionResult Lidos()
+        {
+            ViewBag.Livros = _repo.Lidos.Livros;
+            return View("lista");
+        }
+
+        public string Teste()
+        {
+            return "Nova funcionalidade foi implementada!";
+        }
+    }
+}
